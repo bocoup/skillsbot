2309b014db1d13fa63aa3b7fa0fa46af3acf5895
Ben Alman <cowboy@rj3.net>
2016-07-12 17:19:40 -0400

    Display useful message when no skill categories are defined.

--- src/bot/commands/list.js
+++ src/bot/commands/list.js
@@ -6,6 +6,9 @@ export default createCommand({
   description: 'List all skills, grouped by category.',
 }, (message, {token}) => {
   return query.skillsByCategory({token}).then(categories => {
+    if (categories.length === 0) {
+      return 'No skill categories have been defined for your team yet.';
+    }
     return categories.map(({name, skills}) => {
       return `*${name}*\n> ${skills.join(', ')}`;
     });
