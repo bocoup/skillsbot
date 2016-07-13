c3d04231a51d5391b69b0bbb4f0e0abde5d10d19
Jenn Schiffer <jenn@dotbiz.info>
2016-07-13 15:52:58 -0400

    remove restrict

--- migrations/iql6bkdk.skill-category-is-activated.sql
+++ migrations/iql6bkdk.skill-category-is-activated.sql
@@ -1,5 +1,5 @@
 ALTER TABLE skill ADD COLUMN is_active BOOLEAN NOT NULL DEFAULT true;
 ALTER TABLE skill_category ADD COLUMN is_active BOOLEAN NOT NULL DEFAULT true;
 ---
-ALTER TABLE skill DROP COLUMN is_active RESTRICT;
-ALTER TABLE skill_category DROP COLUMN is_active RESTRICT;
+ALTER TABLE skill DROP COLUMN is_active;
+ALTER TABLE skill_category DROP COLUMN is_active;
