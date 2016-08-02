e5f87ef8a4010528765000d41b98e94c04f6c399
Ben Alman <cowboy@rj3.net>
2016-08-02 16:50:45 -0400

    The "list" command should show skills in deactivated categories.

--- src/sql/skills_by_category.sql
+++ src/sql/skills_by_category.sql
@@ -1,9 +1,31 @@
-SELECT
-  cat.name AS name,
-  ARRAY_AGG(sk.name ORDER BY sk.name) AS skills
-FROM skill sk
-INNER JOIN skill_category cat ON (sk.skill_category_id = cat.id)
-INNER JOIN slack_team team ON (cat.slack_team_id = team.id)
-WHERE team.token = ${token} AND cat.parent_id IS NULL
-GROUP BY cat.name
-ORDER BY cat.name
+WITH skills_categories AS (
+  SELECT
+    sk.name AS skill,
+    cat.name AS name,
+    cat.is_active AS category_is_active
+  FROM skill sk
+  INNER JOIN skill_category cat ON (sk.skill_category_id = cat.id)
+  INNER JOIN slack_team team ON (cat.slack_team_id = team.id)
+  WHERE team.token = ${token}
+  AND cat.parent_id IS NULL
+)
+SELECT name, skills FROM (
+  (
+    SELECT
+      name,
+      1 AS sort,
+      ARRAY_AGG(skill ORDER BY skill) AS skills
+    FROM skills_categories
+    WHERE category_is_active = true
+    GROUP BY name
+  ) UNION (
+    SELECT
+      'Uncategorized' AS name,
+      2 AS sort,
+      ARRAY_AGG(skill ORDER BY skill) AS skills
+    FROM skills_categories
+    WHERE category_is_active = false
+    HAVING COUNT(*) > 1
+  )
+  ORDER BY sort, name
+) tbl
